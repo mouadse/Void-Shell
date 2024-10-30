cdd2d716fd863a17b1748ec41f4fff6f190f6fe2
mouadse <mouadse@gmail.com>
2024-10-30 02:19:14 +0100

    More funcs to be added for our env engine later tomorrow

--- src/env_manager/environment.c
+++ src/env_manager/environment.c
@@ -6,8 +6,23 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 01:51:48 by msennane          #+#    #+#             */
-/*   Updated: 2024/10/30 01:52:14 by msennane         ###   ########.fr       */
+/*   Updated: 2024/10/30 02:18:30 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+
+t_env_var *create_env_var(char *key, char *value)
+{
+	t_env_var *new;
+
+	new = malloc(sizeof(t_env_var));
+	if (!new)
+		return (NULL);
+	new->key = key;
+	new->value = value;
+	new->next = NULL;
+	return (new);
+}
+
