935b9fbe1418e77483fc1f1c220ccfd3344b5cb7
mouadse <mouadse@gmail.com>
2024-10-30 02:04:31 +0100

    Memory management utils for the env variables in our shell

--- src/env_manager/environment_utils.c
+++ src/env_manager/environment_utils.c
@@ -1,14 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   environment.c                                      :+:      :+:    :+:   */
+/*   environment_utils.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 01:51:48 by msennane          #+#    #+#             */
-/*   Updated: 2024/10/30 01:52:14 by msennane         ###   ########.fr       */
+/*   Updated: 2024/10/30 02:03:38 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+char *get_env_value(char *key, t_env_var *env) {
+  t_env_var *head;
+
+  head = env;
+  while (head != NULL) {
+    if (ft_strcmp(head->key, key) == 0)
+      return (head->value);
+    head = head->next;
+  }
+  return (NULL);
+}
+
+void free_env_node(t_env_var *env) {
+  if (!env)
+    return;
+  if (env->key)
+    free(env->key);
+  if (env->value)
+    free(env->value);
+  free(env);
+}
+
+void free_env(t_env_var *env) {
+  t_env_var *head;
+  t_env_var *tmp;
+
+  head = env;
+  while (head != NULL) {
+    tmp = head;
+    head = head->next;
+    free_env_node(tmp);
+  }
+}
