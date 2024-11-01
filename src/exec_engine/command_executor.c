21d5b5c4c596663270a16d0cc19b59093c100a88
mouadse <mouadse@gmail.com>
2024-11-02 00:47:52 +0100

    Added the logic behind our execution engine.

--- src/exec_engine/command_executor.c
+++ src/exec_engine/command_executor.c
@@ -11,3 +11,21 @@
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+void	execute_command(t_command *cmd, t_shell_context *context,
+		int *exit_status)
+{
+	if (CMD_EXEC == cmd->type)
+	{
+		// run exec cmd
+	}
+	else if (CMD_REDIR == cmd->type)
+	{
+		// run our redir
+	}
+	else if (CMD_PIPE == cmd->type)
+	{
+		// pipe handler function here
+	}
+	exit(*exit_status);
+}
