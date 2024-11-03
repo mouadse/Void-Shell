5d7b0e22429be4b4f4a9ea88cd28fed103addee6
mouadse <mouadse@gmail.com>
2024-11-03 17:23:52 +0100

    Fixed our queue header and added the second interfaces for our DSA to be coded later

--- include/queue.h
+++ include/queue.h
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 00:35:15 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/03 16:46:16 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/03 17:23:08 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,40 @@
 
 typedef struct s_node
 {
-	void			*data;
-	struct s_node	*next;
-}					t_node;
+	void				*data;
+	struct s_node		*next;
+}						t_node;
 
 typedef struct s_queue
 {
-	t_node			*front;
-	t_node			*rear;
-}					t_queue;
+	t_node				*front;
+	t_node				*rear;
+}						t_queue;
 
-void init_queue(t_queue *queue);
-void enqueue(t_queue *queue, void *data);
-void enqueue_str(t_queue *queue, char *data);
-void *dequeue(t_queue *queue);
-void *queue_str_convert(t_queue *queue);
-void free_queue(t_queue *queue);
+void					init_queue(t_queue *queue);
+void					enqueue(t_queue *queue, void *data);
+void					*dequeue(t_queue *queue);
+void					*queue_str_convert(t_queue *queue);
+void					free_queue(t_queue *queue);
+
+/* =====	============= 2nd Queue type interfaces ================== */
+
+typedef struct s_node_char
+{
+	char				data;
+	struct s_node_char	*next;
+}						t_node_char;
+
+typedef struct s_queue_char
+{
+	t_node_char			*front;
+	t_node_char			*rear;
+}						t_queue_char;
+
+void					init_queue_char(t_queue_char *queue);
+void					enqueue_char(t_queue_char *queue, char data);
+void					enqueue_str(t_queue_char *queue, char *str);
+char					dequeue_char(t_queue_char *queue);
+char					*queue_char_str_convert(t_queue_char *queue);
 
 #endif
