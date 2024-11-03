5873822944c2e46780c751b84c690587a5b70699
mouadse <mouadse@gmail.com>
2024-11-03 16:39:41 +0100

    Added a heredoc file to parse and process the heredoc inputs

--- include/exec_engine.h
+++ include/exec_engine.h
@@ -6,15 +6,21 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 01:40:43 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/02 17:06:16 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/02 18:47:34 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXEC_ENGINE_H
 # define EXEC_ENGINE_H
 
+#include "command.h"
+
 # define SHELL_CHILD_PID_FILE "/tmp/shell_subprocess_pid.tmp"
 # define SHELL_CHILD_STATUS_FILE "/tmp/shell_exit_status.tmp"
 # define SHELL_HEREDOC_FILE "/tmp/shell_heredoc_content.tmp"
+
+void	execute_pipeline_command(t_command *cmd, t_shell_context *context,
+		int *exit_status);
+
 
 #endif
--- src/exec_engine/heredoc.c
+++ src/exec_engine/heredoc.c
@@ -1,31 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   command_executor.c                                 :+:      :+:    :+:   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 23:46:19 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/02 18:46:57 by msennane         ###   ########.fr       */
+/*   Created: 2024/11/03 16:39:23 by msennane          #+#    #+#             */
+/*   Updated: 2024/11/03 16:39:29 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	execute_command(t_command *cmd, t_shell_context *context,
-		int *exit_status)
-{
-	if (CMD_EXEC == cmd->type)
-	{
-		// run exec cmd
-	}
-	else if (CMD_REDIR == cmd->type)
-	{
-		// run our redir
-	}
-	else if (CMD_PIPE == cmd->type)
-	{
-		execute_pipeline_command(cmd, context, exit_status);
-	}
-	exit(*exit_status);
-}
+
