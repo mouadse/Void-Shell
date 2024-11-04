fa2b6cd75f18f427506ab8c2814448b6a67404e1
Mouad <mouadse@gmail.com>
2024-11-04 17:42:57 +0100

    Finished the pipe executor func
    Only thing I still need to implement is better exit and resources
    cleanup function since our void shell lacks this for now

--- src/exec_engine/pipe_handler.c
+++ src/exec_engine/pipe_handler.c
@@ -6,94 +6,105 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 23:46:29 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/04 17:24:58 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/04 17:41:54 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+#include <fcntl.h>
+#include <time.h>
 
-static void save_exit_status(t_command *cmd, int status_code) {
-  int fd;
-  ssize_t bytes_written;
-  fd = open(SHELL_CHILD_STATUS_FILE, O_WRONLY | O_CREAT | O_APPEND, 0777);
-  if (fd < 0) {
-    perror("open");
-    exit(EXIT_FAILURE); // better cleanup function to be added here
-  }
-  bytes_written = write(fd, &status_code, sizeof(status_code));
-  if (bytes_written < 0) {
-    perror("write");
-    exit(EXIT_FAILURE); // better cleanup function to be added here
-  }
-  close(fd);
+static void	save_exit_status(t_command *cmd, int status_code)
+{
+	int		fd;
+	ssize_t	bytes_written;
+
+	fd = open(SHELL_CHILD_STATUS_FILE, O_WRONLY | O_CREAT | O_APPEND, 0777);
+	if (fd < 0)
+	{
+		perror("open");
+		exit(EXIT_FAILURE); // better cleanup function to be added here
+	}
+	bytes_written = write(fd, &status_code, sizeof(status_code));
+	if (bytes_written < 0)
+	{
+		perror("write");
+		exit(EXIT_FAILURE); // better cleanup function to be added here
+	}
+	close(fd);
 }
 
-static void left_pipe(t_command *cmd, int *fd, int *exit_status) {
-  close(fd[0]);
-  dup2(fd[1], STDOUT_FILENO);
-  close(fd[1]);
-  //   execute_command(cmd, exit_status);
-  exit(EXIT_SUCCESS);
+static void	left_pipe(t_command *cmd, int *fd, int *exit_status)
+{
+	close(fd[0]);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[1]);
+	//   execute_command(cmd, exit_status);
+	exit(EXIT_SUCCESS);
 }
 
-static void right_pipe(t_command *cmd, int *fd, int *exit_status) {
-  close(fd[1]);
-  dup2(fd[0], STDIN_FILENO);
-  close(fd[0]);
-  //   execute_command(cmd, exit_status);
-  exit(EXIT_SUCCESS);
+static void	right_pipe(t_command *cmd, int *fd, int *exit_status)
+{
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
+	//   execute_command(cmd, exit_status);
+	exit(EXIT_SUCCESS);
 }
 
-static void signal_handler(void) {
-  signal(SIGINT, SIG_IGN);
-  signal(SIGQUIT, SIG_IGN);
+static void	signal_handler(void)
+{
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
 }
 
-void execute_pipeline_command(t_command *cmd, t_shell_context *context,
-                              int *exit_status) {
-  int fd[2];
-  pid_t left_child;
-  pid_t right_child;
-  int status;
-  int heredoc_flag;
-  t_pipe *pipe_cmd;
+void	execute_pipeline_command(t_command *cmd, t_shell_context *context,
+		int *exit_status)
+{
+	int		fd[2];
+	pid_t	pids[2];
+	int		status;
+	int		heredoc_flag;
+	t_pipe	*pipe_cmd;
 
-  pipe_cmd = (t_pipe *)cmd;
-  if (pipe(fd) < 0)
-    perror("Pipe");
-  left_child = fork();
-  if (!left_child) {
-    close(fd[0]);
-    dup2(fd[1], STDOUT_FILENO);
-    close(fd[1]);
-    // run_cmd
-    exit(EXIT_SUCCESS);
-  }
-  if (pipe_cmd->left->type == CMD_REDIR &&
-      ((t_redir *)pipe_cmd->left)->redir_type == '%') {
-    close(fd[0]);
-    close(fd[1]);
-    waitpid(left_child, NULL, 0);
-    heredoc_flag = 1;
-  }
-  right_child = fork();
-  if (!right_child) {
-    close(fd[1]);
-    dup2(fd[0], STDIN_FILENO);
-    close(fd[0]);
-    // run_cmd
-    exit(EXIT_SUCCESS);
-  }
-  if (!heredoc_flag) {
-    close(fd[0]);
-    close(fd[1]);
-    waitpid(left_child, NULL, 0);
-  }
-  waitpid(right_child, &status, 0);
-  if (WIFEXITED(status))
-    *exit_status = WEXITSTATUS(status);
-  else
-    *exit_status = EXIT_FAILURE;
-  // Save the exit status somewhere
-  // free and exit
+	pipe_cmd = (t_pipe *)cmd;
+	pipe(fd);
+	pids[0] = fork(); // for left command
+	if (pids[0] < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE); // to be handled better for potential cleanup
+	}
+	if (!pids[0])
+		left_pipe(pipe_cmd->left, fd, exit_status);
+	if (pipe_cmd->right->type == CMD_REDIR
+		&& ((t_redir *)pipe_cmd->left)->redir_type == '%')
+	{
+		heredoc_flag = 1;
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pids[0], NULL, 0);
+	}
+	pids[1] = fork(); // for right command
+	if (pids[1] < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE); // to be handled better for potential cleanup
+	}
+	if (!pids[1])
+		right_pipe(pipe_cmd->right, fd, exit_status);
+	signal_handler();
+	if (!heredoc_flag)
+	{
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pids[0], NULL, 0);
+	}
+	waitpid(pids[1], &status, 0);
+	if (WIFEXITED(status))
+		*exit_status = WEXITSTATUS(status);
+	else
+		*exit_status = 1;
+	save_exit_status(cmd, *exit_status);
+	// free and exit our child processes
 }
