26da40d51a694135fdba1b7564ff28415f20a2cd
Mouad <mouadse@gmail.com>
2024-11-05 02:01:43 +0100

    Added a function to remove empty args from our buffer

--- src/exec_engine/execution_utils.c
+++ src/exec_engine/execution_utils.c
@@ -12,6 +12,20 @@
 
 #include "../../include/minishell.h"
 
+void clean_empty_arguments(t_exec *exec_cmd) {
+  int i;
+
+  i = 0;
+  while (exec_cmd->argv[0] && exec_cmd->argv[0][0] != '\0') {
+    i = 0;
+    while (exec_cmd->argv[i] && exec_cmd->argv[i + 1]) {
+      exec_cmd->argv[i] = exec_cmd->argv[i + 1];
+      i++;
+    }
+    exec_cmd->argv[i] = NULL;
+  }
+}
+
 char *get_executable_path(char *command, char *path) {
   int i;
   char **path_directories;
