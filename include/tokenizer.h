911f91ae2d0577c6b49465b803509260760862cf
mouadse <mouadse@gmail.com>
2024-11-06 20:29:01 +0100

    Norm fixed for the header file

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -6,11 +6,14 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 20:18:05 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/06 20:18:17 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/06 20:28:37 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENIZER_H
-#define TOKENIZER_H
+# define TOKENIZER_H
+
+int	peek(char **ps, char *es, char *toks);
+int	gettoken(char **ps, char *es, char **q, char **eq);
 
 #endif
