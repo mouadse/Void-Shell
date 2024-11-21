f0e869dcbdddbe1df645faa74b716819d6fee5c6
Mouad <mouadse@gmail.com>
2024-11-21 13:09:36 +0100

    More memory cleanup funcs added

--- src/utils/memory_cleanup.c
+++ src/utils/memory_cleanup.c
@@ -6,37 +6,50 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 12:29:51 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/21 12:50:50 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/21 13:09:17 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-
-static void cleanup_resources(t_shell_context *context)
+static void	cleanup_resources(t_shell_context *context)
 {
-//   if (context->input)
-//     ft_free(context->input);
-//   if (context->tree)
-//     // free_ast(context->tree);
-//   if (context->
+	if (context->input)
+		ft_free(context->input);
+	if (context->tree)
+	{
+		release_command_resources(context->tree);
+		context->tree = NULL;
+	}
+	if (context->env_vars)
+	{
+		free_env(context->env_vars);
+		context->env_vars = NULL;
+	}
+	if (context->queue)
+	{
+		free_queue(context->queue);
+		context->queue = NULL;
+	}
 }
 
-
-void ft_free(void *ptr) {
-	if (ptr) {
+void	ft_free(void *ptr)
+{
+	if (ptr)
+	{
 		free(ptr);
 		ptr = NULL;
 	}
 }
 
-void terminate_cleanly(t_shell_context *context, int status)
+void	terminate_cleanly(t_shell_context *context, int status)
 {
 	cleanup_resources(context);
 	exit(status);
 }
 
-void terminate_with_error(t_shell_context *context, char *message, int status)
+void	terminate_with_error(t_shell_context *context, char *message,
+		int status)
 {
 	cleanup_resources(context);
 	perror(message);
