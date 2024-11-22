97cd95c6ce476f6e2210ce068ea31a7f0138127f
Mouad <mouadse@gmail.com>
2024-11-22 12:43:38 +0100

    Working on the noise part in our vodid-shell aka signal handling

--- src/signals/noise.c
+++ src/signals/noise.c
@@ -6,8 +6,12 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 01:31:03 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/01 01:31:35 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/22 12:43:10 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+// This is the part responsible for the noise in the shell aka signal handling
+// which involves 3 steps : Ignore signals, Handle signals, Default signals
+
