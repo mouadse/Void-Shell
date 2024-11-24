d011902a33c001e0c90c7644e7d9ae9ed17106e5
Mouad <mouadse@gmail.com>
2024-11-25 00:33:49 +0100

    Added a function to save the subprocess child pid

--- src/utils/void-shell-utils1.c
+++ src/utils/void-shell-utils1.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 18:39:02 by msennane          #+#    #+#             */
-/*   Updated: 2024/11/24 23:54:46 by msennane         ###   ########.fr       */
+/*   Updated: 2024/11/25 00:33:16 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,4 +35,16 @@ void	retrieve_exit_status(t_command *cmd, t_shell_context *context,
 			terminate_with_error(context, "read", 1);
 		close(fd);
 	}
+}
+
+void	store_subprocess_pid(pid_t pid, t_shell_context *context)
+{
+	int	fd;
+
+	fd = open(SHELL_CHILD_PID_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (-1 == fd)
+		terminate_with_error(context, "open", 1);
+	if (-1 == write(fd, &pid, sizeof(pid_t)))
+		terminate_with_error(context, "write", 1);
+	close(fd);
 }
