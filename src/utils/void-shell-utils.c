52c74c1038f9cad5bde1836532217165bee9345c
Mouad <mouadse@gmail.com>
2024-12-06 01:27:16 +0100

    Fixed output err

--- src/utils/void-shell-utils.c
+++ src/utils/void-shell-utils.c
@@ -12,55 +12,47 @@
 
 #include "../../include/minishell.h"
 
-int	is_whitespace(char c)
-{
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
-		|| c == '\r')
-		return (1);
-	return (0);
+int is_whitespace(char c) {
+  if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r')
+    return (1);
+  return (0);
 }
 
-int	is_whitespace_string(char *str)
-{
-	int	i;
+int is_whitespace_string(char *str) {
+  int i;
 
-	i = 0;
-	while (str[i])
-	{
-		if (!is_whitespace(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
+  i = 0;
+  while (str[i]) {
+    if (!is_whitespace(str[i]))
+      return (0);
+    i++;
+  }
+  return (1);
 }
 
-int	is_numeric(const char *str)
-{
-	if (*str && (*str == '+' || *str == '-'))
-		str++;
-	if (*str == '\0')
-		return (0);
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-			return (0);
-		str++;
-	}
-	return (1);
+int is_numeric(const char *str) {
+  if (*str && (*str == '+' || *str == '-'))
+    str++;
+  if (*str == '\0')
+    return (0);
+  while (*str) {
+    if (*str < '0' || *str > '9')
+      return (0);
+    str++;
+  }
+  return (1);
 }
 
-int	ft_fork(t_shell_context *context)
-{
-	int	pid;
+int ft_fork(t_shell_context *context) {
+  int pid;
 
-	pid = fork();
-	if (pid == -1)
-		terminate_with_error(context, "fork", 1);
-	return (pid);
+  pid = fork();
+  if (pid == -1)
+    terminate_with_error(context, "fork", 1);
+  return (pid);
 }
 
-void	ft_pipe(int fd[2], t_shell_context *context)
-{
-	if (pipe(fd) == -1)
-		terminate_with_error(context, "fork", 1);
+void ft_pipe(int fd[2], t_shell_context *context) {
+  if (pipe(fd) == -1)
+    terminate_with_error(context, "pipe", 1);
 }
