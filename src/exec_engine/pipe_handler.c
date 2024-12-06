4da7ab2bb75798640225f6843055ba4c1816d97d
Mouad <mouadse@gmail.com>
2024-12-06 13:44:07 +0100

    Segfaults everywhere !!!

--- src/exec_engine/pipe_handler.c
+++ src/exec_engine/pipe_handler.c
@@ -6,48 +6,208 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 23:46:29 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/06 12:43:58 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/06 13:39:40 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	save_exit_status(t_shell_context *context, int status_code)
+// void	save_exit_status(t_shell_context *context, int status_code)
+// {
+// 	int		fd;
+// 	ssize_t	bytes_written;
+
+// 	fd = open(SHELL_CHILD_STATUS_FILE, O_WRONLY | O_CREAT | O_APPEND, 0777);
+// 	if (fd < 0)
+// 	{
+// 		perror("open");
+// 		terminate_with_error(context, "Open", EXIT_FAILURE);
+// 	}
+// 	bytes_written = write(fd, &status_code, sizeof(status_code));
+// 	if (bytes_written < 0)
+// 	{
+// 		perror("write");
+// 		terminate_with_error(context, "Write", EXIT_FAILURE);
+// 	}
+// 	ft_close(context, fd);
+// }
+
+// void	left_pipe(t_shell_context *context, t_command *cmd, int fd[2],
+// 		int *exit_status)
+// {
+// 	ft_close(context, fd[0]);
+// 	dup2(fd[1], STDOUT_FILENO);
+// 	ft_close(context, fd[1]);
+// 	execute_command(cmd, context, exit_status);
+// 	exit(0);
+// }
+
+// void	right_pipe(t_command *cmd, t_shell_context *context, int fd[2],
+// 		int *exit_status)
+// {
+// 	ft_close(context, fd[1]);
+// 	dup2(fd[0], STDIN_FILENO);
+// 	ft_close(context, fd[0]);
+// 	execute_command(cmd, context, exit_status);
+// 	exit(0);
+// }
+
+// Executes the left subtree of the pipeline
+static void	execute_left_subtree(t_command *cmd, int fd[2],
+		t_shell_context *context, int *exit_status)
 {
-	int		fd;
-	ssize_t	bytes_written;
+	// Redirect stdout to the write end of the pipe
+	close(fd[0]);
+	if (dup2(fd[1], STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		terminate_with_error(context, "dup2", 1);
+	}
+	close(fd[1]);
+	// Execute the left command
+	run_exec(cmd, context, exit_status);
+	// Exit the child process
+	exit(0);
+}
+
+// Executes the right subtree of the pipeline
+static void	execute_right_subtree(t_command *cmd, int fd[2],
+		t_shell_context *context, int *exit_status)
+{
+	// Redirect stdin to the read end of the pipe
+	close(fd[1]);
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+	{
+		perror("dup2");
+		terminate_with_error(context, "dup2", 1);
+	}
+	close(fd[0]);
+	// Execute the right command
+	run_exec(cmd, context, exit_status);
+	// Exit the child process
+	exit(0);
+}
+
+// Writes the exit status to a temporary file
+static void	write_exit_status_to_file(t_shell_context *context, int exit_status)
+{
+	int	fd;
 
 	fd = open(SHELL_CHILD_STATUS_FILE, O_WRONLY | O_CREAT | O_APPEND, 0777);
 	if (fd < 0)
 	{
 		perror("open");
-		terminate_with_error(context, "Open", EXIT_FAILURE);
+		terminate_with_error(context, "open", 1);
 	}
-	bytes_written = write(fd, &status_code, sizeof(status_code));
-	if (bytes_written < 0)
+	if (write(fd, &exit_status, sizeof(int)) < 0)
 	{
 		perror("write");
-		terminate_with_error(context, "Write", EXIT_FAILURE);
+		close(fd);
+		terminate_with_error(context, "write", 1);
 	}
-	ft_close(context, fd);
+	close(fd);
 }
 
-void	left_pipe(t_shell_context *context, t_command *cmd, int fd[2],
-		int *exit_status)
+// Sets up signal handlers to ignore SIGINT and SIGQUIT
+static void	set_signals(void)
 {
-	ft_close(context, fd[0]);
-	dup2(fd[1], STDOUT_FILENO);
-	ft_close(context, fd[1]);
-	execute_command(cmd, context, exit_status);
-	exit(0);
+	if (signal(SIGINT, SIG_IGN) == SIG_ERR)
+	{
+		perror("signal");
+		exit(EXIT_FAILURE);
+	}
+	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+	{
+		perror("signal");
+		exit(EXIT_FAILURE);
+	}
 }
 
-void	right_pipe(t_command *cmd, t_shell_context *context, int fd[2],
+// Executes a pipeline command
+void	execute_pipeline_command(t_command *cmd, t_shell_context *context,
 		int *exit_status)
 {
-	ft_close(context, fd[1]);
-	dup2(fd[0], STDIN_FILENO);
-	ft_close(context, fd[0]);
-	execute_command(cmd, context, exit_status);
-	exit(0);
+	int		fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	int		status;
+	int		is_herdoc;
+	t_pipe	*pcmd;
+
+	is_herdoc = 0;
+	pcmd = (t_pipe *)cmd;
+	// Create a pipe
+	if (pipe(fd) == -1)
+	{
+		perror("pipe");
+		terminate_with_error(context, "pipe", 1);
+	}
+	// Fork the first child process for the left subtree
+	pid1 = fork();
+	if (pid1 < 0)
+	{
+		perror("fork");
+		terminate_with_error(context, "fork", 1);
+	}
+	if (pid1 == 0)
+	{
+		// Child process executes the left subtree
+		execute_left_subtree(pcmd->left, fd, context, exit_status);
+	}
+	// Check if the left command is a heredoc
+	if (pcmd->left->type == CMD_REDIR
+		&& ((t_redir *)pcmd->left)->redir_type == '%')
+	{
+		// Close both ends of the pipe in the parent
+		// close(fd[0]);
+		// close(fd[1]);
+		// Wait for the first child process
+		if (waitpid(pid1, NULL, 0) == -1)
+		{
+			perror("waitpid");
+			terminate_with_error(context, "waitpid", 1);
+		}
+		is_herdoc = 1;
+	}
+	// Fork the second child process for the right subtree
+	pid2 = fork();
+	if (pid2 < 0)
+	{
+		perror("fork");
+		terminate_with_error(context, "fork", 1);
+	}
+	if (pid2 == 0)
+	{
+		// Child process executes the right subtree
+		execute_right_subtree(pcmd->right, fd, context, exit_status);
+	}
+	// Set up signal handlers in the parent
+	set_signals();
+	if (!is_herdoc)
+	{
+		// Close both ends of the pipe in the parent if not heredoc
+		close(fd[0]);
+		close(fd[1]);
+		// Wait for the first child process
+		if (waitpid(pid1, NULL, 0) == -1)
+		{
+			perror("waitpid");
+			terminate_with_error(context, "waitpid", 1);
+		}
+	}
+	// Wait for the second child process
+	if (waitpid(pid2, &status, 0) == -1)
+	{
+		perror("waitpid");
+		terminate_with_error(context, "waitpid", 1);
+	}
+	// Update the exit status based on the second child process
+	if (WIFEXITED(status))
+		*exit_status = WEXITSTATUS(status);
+	else
+		*exit_status = 1;
+	// Write the exit status to a temporary file
+	write_exit_status_to_file(context, *exit_status);
+	// Clean up and exit the shell with the appropriate status
+	terminate_cleanly(context, *exit_status);
 }
--- src/exec_engine/pipe_handler2.c
+++ src/exec_engine/pipe_handler2.c
@@ -6,90 +6,90 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 23:46:29 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/06 13:10:09 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/06 13:30:17 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	signal_handler(void)
-{
-	signal(SIGINT, SIG_IGN);
-	signal(SIGQUIT, SIG_IGN);
-}
+// static void	signal_handler(void)
+// {
+// 	signal(SIGINT, SIG_IGN);
+// 	signal(SIGQUIT, SIG_IGN);
+// }
 
-static void	handle_heredoc(pid_t pid1, int fd[2], t_shell_context *context,
-		int *exit_status)
-{
-	int	status;
+// static void	handle_heredoc(pid_t pid1, int fd[2], t_shell_context *context,
+// 		int *exit_status)
+// {
+// 	int	status;
 
-	waitpid(pid1, &status, 0);
-	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-	{
-		ft_close(context, fd[0]);
-		ft_close(context, fd[1]);
-		*exit_status = WEXITSTATUS(status);
-	}
-}
+// 	waitpid(pid1, &status, 0);
+// 	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+// 	{
+// 		ft_close(context, fd[0]);
+// 		ft_close(context, fd[1]);
+// 		*exit_status = WEXITSTATUS(status);
+// 	}
+// }
 
-static pid_t	handle_pipeline_child_processes(t_pipe *pipe_cmd, int fd[2],
-		t_shell_context *context, int *exit_status)
-{
-	pid_t	pid1;
+// static pid_t	handle_pipeline_child_processes(t_pipe *pipe_cmd, int fd[2],
+// 		t_shell_context *context, int *exit_status)
+// {
+// 	pid_t	pid1;
 
-	pid1 = ft_fork(context);
-	if (pid1 == 0)
-	{
-		if (pipe_cmd->left->type == CMD_REDIR
-			&& ((t_redir *)pipe_cmd->left)->redir_type == '%')
-		{
-			ft_close(context, fd[0]);
-			dup2(fd[1], STDOUT_FILENO);
-			ft_close(context, fd[1]);
-			execute_command(pipe_cmd->left, context, exit_status);
-		}
-		else
-			left_pipe(context, pipe_cmd->left, fd, exit_status);
-	}
-	if (pipe_cmd->left->type == CMD_REDIR
-		&& ((t_redir *)pipe_cmd->left)->redir_type == '%')
-	{
-		handle_heredoc(pid1, fd, context, exit_status);
-	}
-	return (pid1);
-}
+// 	pid1 = ft_fork(context);
+// 	if (pid1 == 0)
+// 	{
+// 		if (pipe_cmd->left->type == CMD_REDIR
+// 			&& ((t_redir *)pipe_cmd->left)->redir_type == '%')
+// 		{
+// 			ft_close(context, fd[0]);
+// 			dup2(fd[1], STDOUT_FILENO);
+// 			ft_close(context, fd[1]);
+// 			execute_command(pipe_cmd->left, context, exit_status);
+// 		}
+// 		else
+// 			left_pipe(context, pipe_cmd->left, fd, exit_status);
+// 	}
+// 	if (pipe_cmd->left->type == CMD_REDIR
+// 		&& ((t_redir *)pipe_cmd->left)->redir_type == '%')
+// 	{
+// 		handle_heredoc(pid1, fd, context, exit_status);
+// 	}
+// 	return (pid1);
+// }
 
-void	execute_pipeline_command(t_command *cmd, t_shell_context *context,
-		int *exit_status)
-{
-	int		fd[2];
-	int		status;
-	t_pipe	*pipe_cmd;
+// void	execute_pipeline_command(t_command *cmd, t_shell_context *context,
+// 		int *exit_status)
+// {
+// 	int		fd[2];
+// 	int		status;
+// 	t_pipe	*pipe_cmd;
 
-	pid_t pid1, pid2;
-	pipe_cmd = (t_pipe *)cmd;
-	ft_pipe(fd, context);
-	pid1 = handle_pipeline_child_processes(pipe_cmd, fd, context, exit_status);
-	pid2 = ft_fork(context);
-	if (pid2 == 0)
-		right_pipe(pipe_cmd->right, context, fd, exit_status);
-	signal_handler();
-	ft_close(context, fd[0]);
-	ft_close(context, fd[1]);
-	waitpid(pid2, &status, 0);
-	if (WIFEXITED(status))
-		*exit_status = WEXITSTATUS(status);
-	else
-		*exit_status = 1;
-	if (!(pipe_cmd->left->type == CMD_REDIR
-			&& ((t_redir *)pipe_cmd->left)->redir_type == '%'))
-	{
-		waitpid(pid1, &status, 0);
-		if (WIFEXITED(status))
-			*exit_status = WEXITSTATUS(status);
-		else
-			*exit_status = 1;
-	}
-	save_exit_status(context, *exit_status);
-	terminate_cleanly(context, *exit_status);
-}
+// 	pid_t pid1, pid2;
+// 	pipe_cmd = (t_pipe *)cmd;
+// 	ft_pipe(fd, context);
+// 	pid1 = handle_pipeline_child_processes(pipe_cmd, fd, context, exit_status);
+// 	pid2 = ft_fork(context);
+// 	if (pid2 == 0)
+// 		right_pipe(pipe_cmd->right, context, fd, exit_status);
+// 	signal_handler();
+// 	ft_close(context, fd[0]);
+// 	ft_close(context, fd[1]);
+// 	waitpid(pid2, &status, 0);
+// 	if (WIFEXITED(status))
+// 		*exit_status = WEXITSTATUS(status);
+// 	else
+// 		*exit_status = 1;
+// 	if (!(pipe_cmd->left->type == CMD_REDIR
+// 			&& ((t_redir *)pipe_cmd->left)->redir_type == '%'))
+// 	{
+// 		waitpid(pid1, &status, 0);
+// 		if (WIFEXITED(status))
+// 			*exit_status = WEXITSTATUS(status);
+// 		else
+// 			*exit_status = 1;
+// 	}
+// 	save_exit_status(context, *exit_status);
+// 	terminate_cleanly(context, *exit_status);
+// }
