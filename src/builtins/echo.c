e70c4d7970b3e8bedf3640b4176716295946fc06
Mouad <mouadse@gmail.com>
2024-12-06 19:48:14 +0100

    Updated the echo cmd to handle some edge cases

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,24 +1,33 @@
 #include "../../include/minishell.h"
 
-void	echo(char **args)
-{
-	int	i;
-	int	new_line;
+static int is_n_option(const char *str) {
+  if (str[0] != '-')
+    return 0;
+  int j = 1;
+  while (str[j] == 'n')
+    j++;
+  return str[j] == '\0';
+}
 
-	new_line = 1;
-	i = 1;
-	if (args[1] && ft_strncmp(args[1], "-n", 3) == 0)
-	{
-		new_line = 0;
-		i++;
-	}
-	while (args[i])
-	{
-		ft_putstr_fd(args[i], STDOUT_FILENO);
-		if (args[i + 1])
-			ft_putstr_fd(" ", STDOUT_FILENO);
-		i++;
-	}
-	if (new_line)
-		ft_putstr_fd("\n", STDOUT_FILENO);
+void echo(char **args) {
+  int i;
+  int new_line;
+
+  new_line = 1;
+  i = 1;
+
+  while (args[i] && is_n_option(args[i])) {
+    new_line = 0;
+    i++;
+  }
+
+  while (args[i]) {
+    ft_putstr_fd(args[i], STDOUT_FILENO);
+    if (args[i + 1])
+      ft_putstr_fd(" ", STDOUT_FILENO);
+    i++;
+  }
+
+  if (new_line)
+    ft_putstr_fd("\n", STDOUT_FILENO);
 }
