c99ebce00c574e0c1f501354129f7f9969f29b88
mouadse <mouadse@gmail.com>
2024-12-15 22:54:36 +0100

    Updated the export part to handle the append mode

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -1,30 +1,62 @@
 #include "../../include/minishell.h"
 
-void	ft_export(char **args, t_env_var **env_var_list, int *exit_status)
-{
-	int		i;
-	char	*key;
-	char	*equal_pos;
+// void	ft_export(char **args, t_env_var **env_var_list, int *exit_status)
+// {
+// 	int		i;
+// 	char	*key;
+// 	char	*equal_pos;
 
-	if (!args[1])
-	{
-		display_env_variables(*env_var_list);
-		return ;
-	}
-	i = 0;
-	while (args[++i])
-	{
-		equal_pos = ft_strchr(args[i], '=');
-		key = extract_key(args[i], equal_pos);
-		if (!is_valid_var(key))
-		{
-			ft_putstr_fd("minishell: export: `", 2);
-			ft_putstr_fd(key, 2);
-			ft_putstr_fd("': not a valid identifier\n", 2);
-			*exit_status = 1;
-			i++;
-			continue ;
-		}
-		insert_exported_var(equal_pos, key, env_var_list);
-	}
+// 	if (!args[1])
+// 	{
+// 		display_env_variables(*env_var_list);
+// 		return ;
+// 	}
+// 	i = 0;
+// 	while (args[++i])
+// 	{
+// 		equal_pos = ft_strchr(args[i], '=');
+// 		key = extract_key(args[i], equal_pos);
+// 		if (!is_valid_var(key))
+// 		{
+// 			ft_putstr_fd("minishell: export: `", 2);
+// 			ft_putstr_fd(key, 2);
+// 			ft_putstr_fd("': not a valid identifier\n", 2);
+// 			*exit_status = 1;
+// 			i++;
+// 			continue ;
+// 		}
+// 		insert_exported_var(equal_pos, key, env_var_list);
+// 	}
+// }
+void ft_export(char **args, t_env_var **env_var_list, int *exit_status) {
+  int i;
+  char *key;
+  char *equal_pos;
+  int is_append;
+
+  if (!args[1]) {
+    display_env_variables(*env_var_list);
+    return;
+  }
+  i = 0;
+  while (args[++i]) {
+    is_append = 0;
+    equal_pos = ft_strchr(args[i], '=');
+    if (equal_pos && equal_pos > args[i] && equal_pos[-1] == '+') {
+      is_append = 1;
+      equal_pos[-1] = '\0'; // Temporarily remove '+' for key extraction
+    }
+    key = extract_key(args[i], equal_pos);
+    if (is_append && equal_pos)
+      equal_pos[-1] = '+'; // Restore '+' if it was removed
+
+    if (!is_valid_var(key)) {
+      ft_putstr_fd("minishell: export: `", 2);
+      ft_putstr_fd(key, 2);
+      ft_putstr_fd("': not a valid identifier\n", 2);
+      *exit_status = 1;
+      continue;
+    }
+    insert_exported_var(equal_pos, key, env_var_list);
+  }
 }
--- src/builtins/export_utils.c
+++ src/builtins/export_utils.c
@@ -1,71 +1,86 @@
 #include "../../include/minishell.h"
 
+int is_valid_var(char *key) {
+  int i;
 
-
-int	is_valid_var(char *key)
-{
-	int	i;
-
-	i = 0;
-	if (!ft_isalpha(key[i]) && key[i] != '_')
-		return (0);
-	i++;
-	while (key[i])
-	{
-		if (!ft_isalnum(key[i]) && key[i] != '_')
-			return (0);
-		i++;
-	}
-	return (1);
+  i = 0;
+  if (!ft_isalpha(key[i]) && key[i] != '_')
+    return (0);
+  i++;
+  while (key[i]) {
+    if (!ft_isalnum(key[i]) && key[i] != '_')
+      return (0);
+    i++;
+  }
+  return (1);
 }
 
-char	*extract_key(char *str, char *equal_pos)
-{
-	char	*key;
+char *extract_key(char *str, char *equal_pos) {
+  char *key;
 
-	if (!equal_pos)
-		return (ft_strdup(str));
-	key = ft_substr(str, 0, equal_pos - str);
-	return (key);
+  if (!equal_pos)
+    return (ft_strdup(str));
+  key = ft_substr(str, 0, equal_pos - str);
+  return (key);
 }
 
-void	insert_exported_var(char *equal_sign, char *key,
-		t_env_var **env_var_list)
-{
-	t_env_var	*new_node;
+// void	insert_exported_var(char *equal_sign, char *key,
+// 		t_env_var **env_var_list)
+// {
+// 	t_env_var	*new_node;
 
-	if (!equal_sign)
-	{
-		new_node = create_env_var(key, NULL);
-		insert_env_var(env_var_list, new_node);
-	}
-	else
-	{
-		new_node = create_env_var(key, ft_strdup(equal_sign + 1));
-		insert_env_var(env_var_list, new_node);
-	}
+// 	if (!equal_sign)
+// 	{
+// 		new_node = create_env_var(key, NULL);
+// 		insert_env_var(env_var_list, new_node);
+// 	}
+// 	else
+// 	{
+// 		new_node = create_env_var(key, ft_strdup(equal_sign + 1));
+// 		insert_env_var(env_var_list, new_node);
+// 	}
+// }
+
+void insert_exported_var(char *equal_sign, char *key,
+                         t_env_var **env_var_list) {
+  t_env_var *new_node;
+  char *value;
+  char *existing_value;
+
+  if (!equal_sign) {
+    new_node = create_env_var(key, NULL);
+    insert_env_var(env_var_list, new_node);
+  } else {
+    value = ft_strdup(equal_sign + 1);
+    if (equal_sign[-1] == '+') // Check for += operator
+    {
+      existing_value = get_env_value(key, *env_var_list);
+      if (existing_value) {
+        char *new_value = ft_strjoin(existing_value, value);
+        value = new_value;
+      }
+    }
+    new_node = create_env_var(key, value);
+    insert_env_var(env_var_list, new_node);
+  }
 }
 
-void	display_env_variables(t_env_var *env_var_list)
-{
-	t_env_var	*current;
+void display_env_variables(t_env_var *env_var_list) {
+  t_env_var *current;
 
-	current = env_var_list;
-	while (current)
-	{
-		if (current->value)
-			printf("declare -x %s=\"%s\"\n", current->key, current->value);
-		else
-			printf("declare -x %s\n", current->key);
-		current = current->next;
-	}
+  current = env_var_list;
+  while (current) {
+    if (current->value)
+      printf("declare -x %s=\"%s\"\n", current->key, current->value);
+    else
+      printf("declare -x %s\n", current->key);
+    current = current->next;
+  }
 }
 
-void	simple_export(char **args, t_env_var *env_var_list)
-{
-	if (!args[1])
-	{
-		display_env_variables(env_var_list);
-		return ;
-	}
+void simple_export(char **args, t_env_var *env_var_list) {
+  if (!args[1]) {
+    display_env_variables(env_var_list);
+    return;
+  }
 }
