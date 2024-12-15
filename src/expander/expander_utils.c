77f601d08640861b42fe415970f0c43290adf175
mouadse <mouadse@gmail.com>
2024-12-16 00:41:08 +0100

    X

--- src/expander/expander_utils.c
+++ src/expander/expander_utils.c
@@ -6,11 +6,12 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 23:39:28 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/16 00:39:48 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/16 00:40:58 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+#include <stdio.h>
 int has_special_characters(char *str) {
   if (!str)
     return 0;
@@ -58,7 +59,8 @@ void process_variable(char *str, int *values[2], t_queue_char *queue,
   int *i = values[0];
   char *var_name = extract_variable_name(str + *i);
   if (!var_name) {
-    enqueue_char(queue, '$'); // Keep the dollar sign
+	printf("var_name is NULL\n");
+    enqueue_char(queue, '$');
 	(*i)++;
     return;
   }
