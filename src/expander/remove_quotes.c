066551daa24a0f5eeda3a3d3e938f61759e7f0f0
mouadse <mouadse@gmail.com>
2024-12-20 23:55:40 +0100

    X

--- src/expander/remove_quotes.c
+++ src/expander/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:34:10 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/20 23:54:59 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/20 23:55:37 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,42 +77,46 @@
 //     result[j] = '\0';
 //     return (result);
 // }
-
-char	*remove_quotes(const char *str)
+char *remove_quotes(const char *str)
 {
-	size_t	len;
-	size_t	result_index;
-	bool	in_double_quotes;
-	size_t	i;
-	char	*result;
+    if (str == NULL)
+        return (NULL);
 
-	if (str == NULL)
-		return (NULL);
-	len = strlen(str);
-	result = gc_malloc(len + 1);
-	if (!result)
-		return (ft_strdup(""));
-	result_index = 0;
-	in_double_quotes = false;
-	i = 0;
-	while (i < len)
-	{
-		if (str[i] == '"')
-		{
-			in_double_quotes = !in_double_quotes;
-			/* Don't add the double quote to result */
-		}
-		else if (str[i] == '\'' && !in_double_quotes)
-		{
-			/* If next char is also a quote, skip both */
-			if (i + 1 < len && str[i + 1] == '\'')
-				i++;
-			/* Don't add single quotes to the result */
-		}
-		else
-			result[result_index++] = str[i];
-		i++;
-	}
-	result[result_index] = '\0';
-	return (result);
+    size_t len = strlen(str);
+    char *result = gc_malloc(len + 1);
+    if (!result)
+        return (NULL);  // Changed from returning empty string to NULL for consistency
+
+    size_t result_index = 0;
+    bool in_double_quotes = false;
+    size_t i = 0;
+
+    while (i < len)
+    {
+        // Handle double quotes
+        if (str[i] == '"')
+        {
+            in_double_quotes = !in_double_quotes;
+        }
+        // Handle single quotes when not inside double quotes
+        else if (str[i] == '\'' && !in_double_quotes)
+        {
+            // Skip paired single quotes (keep one of them)
+            if (i + 1 < len && str[i + 1] == '\'')
+            {
+                result[result_index++] = str[i];
+                i++;
+            }
+            // Skip lone single quotes
+        }
+        // Copy all other characters
+        else
+        {
+            result[result_index++] = str[i];
+        }
+        i++;
+    }
+
+    result[result_index] = '\0';
+    return (result);
 }
