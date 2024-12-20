3cf86581bffe3bf09b1e1661eab226c12f3e4154
mouadse <mouadse@gmail.com>
2024-12-20 23:34:59 +0100

    X

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 20:46:40 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/20 00:21:49 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/20 23:34:44 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,9 @@ int			is_ambiguous_redirect(const char *filename);
 // HEREDOC PART
 char	*clean_delimiter(char *str, int *is_quoted);
 char	*replace_quotes_with_x(char *str);
+char	*remove_quotes(const char *str);
+
+// EXPANDER PART
+
 char	*remove_quotes(const char *str);
 #endif
---
+++ src/expander/remove_quotes.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   remove_quotes.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/20 23:34:10 by msennane          #+#    #+#             */
+/*   Updated: 2024/12/20 23:34:55 by msennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+char	*remove_quotes(const char *str)
+{
+	int		i;
+	int		j;
+	int		in_double_quotes;
+	char	*result;
+	int		count;
+	int		k;
+
+	i = 0;
+	j = 0;
+	in_double_quotes = 0;
+	result = gc_malloc(ft_strlen(str) + 1);
+	if (!result)
+		return (ft_strdup(""));
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\"' && !in_double_quotes)
+		{
+			in_double_quotes = 1;
+			i++;
+		}
+		else if (str[i] == '\"' && in_double_quotes)
+		{
+			in_double_quotes = 0;
+			i++;
+		}
+		else if (str[i] == '\'' && !in_double_quotes)
+		{
+			if (str[i + 1] == '\'')
+			{
+				i += 2;
+				continue ;
+			}
+			count = 0;
+			k = i;
+			while (str[k] == '\'')
+			{
+				count++;
+				k++;
+			}
+			if (count > 1 && str[k] != '\0')
+			{
+				while (count > 0)
+				{
+					result[j++] = '\'';
+					i++;
+					count--;
+				}
+			}
+			else
+			{
+				i++;
+				while (str[i] && str[i] != '\'')
+					result[j++] = str[i++];
+				if (str[i] == '\'')
+					i++;
+			}
+		}
+		else
+			result[j++] = str[i++];
+	}
+	result[j] = '\0';
+	return (result);
+}
