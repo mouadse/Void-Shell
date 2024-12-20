67a317078bb71580042c9bb16fa3ab53af3490ab
mouadse <mouadse@gmail.com>
2024-12-21 00:53:01 +0100

    X

--- src/expander/clean_nulls_from_argv.c
+++ src/expander/clean_nulls_from_argv.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 00:47:39 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/21 00:48:59 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/21 00:52:56 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,45 +49,49 @@
 // 	}
 // }
 
-
-// Clean a single string from special characters
 static void clean_string(char *str)
 {
-    size_t len;
+	size_t len;
 
-    if (!str || (str[0] == '\x01' && str[1] == '\0'))
-        return;
-    if (str[0] == '\x01')
-        ft_memmove(str, str + 1, ft_strlen(str));
-    len = ft_strlen(str);
-    while (len > 0 && str[len - 1] == '\x01')
-    {
-        str[len - 1] = '\0';
-        len--;
-    }
+	if (!str)
+		return;
+	// Skip strings that are just '\x01'
+	if (str[0] == '\x01' && str[1] == '\0')
+		return;
+	// Remove leading '\x01'
+	if (str[0] == '\x01')
+		ft_memmove(str, str + 1, ft_strlen(str));
+	// Remove trailing '\x01' characters
+	len = ft_strlen(str);
+	while (len > 0 && str[len - 1] == '\x01')
+	{
+		str[len - 1] = '\0';
+		len--;
+	}
 }
 
-// Clean and reorganize the array
 void    clean_nulls_from_argv(char **argv, int size)
 {
-    int     i;
-    int     j;
+	int     i;
+	int     j;
 
-    i = 0;
-    j = 0;
-    while (i < size)
-    {
-        if (argv[i] != NULL)
-        {
-            clean_string(argv[i]);
-            if (argv[i][0] != '\0')  // Only keep non-empty strings
-            {
-                argv[j] = argv[i];
-                j++;
-            }
-        }
-        i++;
-    }
-    while (j < size)
-        argv[j++] = NULL;
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		if (argv[i] != NULL)
+		{
+			if (argv[i][0] == '\x01' && argv[i][1] == '\0')
+			{
+				i++;
+				continue;
+			}
+			clean_string(argv[i]);
+			argv[j] = argv[i];
+			j++;
+		}
+		i++;
+	}
+	while (j < size)
+		argv[j++] = NULL;
 }
