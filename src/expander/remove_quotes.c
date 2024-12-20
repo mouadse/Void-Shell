83f539e21867dde043e1a2a585ebb7d7d6680766
mouadse <mouadse@gmail.com>
2024-12-21 00:17:12 +0100

    X

--- src/expander/remove_quotes.c
+++ src/expander/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:34:10 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/20 23:58:04 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/21 00:17:07 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,68 +78,67 @@
 //     return (result);
 // }
 
-// Function to handle single quotes
-char handle_single_quote(const char *str, int *i)
-{
-    if (str[*i + 1] == '\'')
-    {
-        *i += 2;
-        return '\0';
-    }
-    else
-    {
+
+bool is_double_quote(char c) {
+    return c == '\"';
+}
+
+bool is_single_quote(char c) {
+    return c == '\'';
+}
+
+void handle_double_quotes_rem(const char *str, int *i, bool *in_double_quotes) {
+    if (is_double_quote(str[*i]) && !*in_double_quotes) {
+        *in_double_quotes = true;
         (*i)++;
-        char c = str[*i];
-        while (str[*i] && str[*i] != '\'')
-            (*i)++;
-        if (str[*i] == '\'')
-            (*i)++;
-        return c;
+    } else if (is_double_quote(str[*i]) && *in_double_quotes) {
+        *in_double_quotes = false;
+        (*i)++;
     }
 }
 
-
-// Function to handle characters
-char handle_character(const char *str, int *i, int in_double_quotes)
-{
-    if (str[*i] == '\'' && !in_double_quotes)
-        return handle_single_quote(str, i);
-    else
-        return str[*i];
-}
-
-int toggle_double_quotes(int in_double_quotes)
-{
-    return !in_double_quotes;
-}
-
-// Function to process the input string
-void process_string(const char *str, char *result)
-{
-    int i;
-    int j;
-    int in_double_quotes;
-
-    i = 0;
-    j = 0;
-    in_double_quotes = 0;
-
-    while (str[i] != '\0')
-    {
-        if (str[i] == '\"')
-            in_double_quotes = toggle_double_quotes(in_double_quotes);
-        else
-            result[j++] = handle_character(str, &i, in_double_quotes);
-        i++;
+void handle_single_quotes_rem(const char *str, int *i, bool in_double_quotes, char *result, int *j) {
+    if (is_single_quote(str[*i]) && !in_double_quotes) {
+        if (is_single_quote(str[*i + 1])) {
+            *i += 2;
+            return;
+        }
+        int count = 0;
+        int k = *i;
+        while (is_single_quote(str[k])) {
+            count++;
+            k++;
+        }
+        if (count > 1 && str[k] != '\0') {
+            while (count > 0) {
+                result[(*j)++] = '\'';
+                (*i)++;
+                count--;
+            }
+        } else {
+            (*i)++;
+            while (str[*i] && !is_single_quote(str[*i]))
+                result[(*j)++] = str[(*i)++];
+            if (is_single_quote(str[*i]))
+                (*i)++;
+        }
+    } else {
+        result[(*j)++] = str[(*i)++];
     }
-    result[j] = '\0';
 }
-char *remove_quotes(const char *str)
-{
+
+char *remove_quotes(const char *str) {
+    int i = 0;
+    int j = 0;
+    bool in_double_quotes = false;
     char *result = gc_malloc(ft_strlen(str) + 1);
     if (!result)
         return ft_strdup("");
 
-    process_string(str, result);
+    while (str[i] != '\0') {
+        handle_double_quotes_rem(str, &i, &in_double_quotes);
+        handle_single_quotes_rem(str, &i, in_double_quotes, result, &j);
+    }
+    result[j] = '\0';
     return result;
 }
