998097b48ac20f61b04d18a22a7494438de54e19
mouadse <mouadse@gmail.com>
2024-12-20 23:10:16 +0100

    Saved some c files as d files for backup before touching the norm and doing big modifications

---
+++ src/expander/files/expander.d
@@ -0,0 +1,305 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expander.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 23:39:15 by msennane          #+#    #+#             */
+/*   Updated: 2024/12/19 19:29:26 by msennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+static void	process_argument(char *arg, t_queue_char *queue, int *exit_status,
+		t_shell_context *context)
+{
+	int	i;
+	int	*values[2];
+	int	prev_i;
+
+	if (!arg || !queue || !exit_status || !context)
+		return ;
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		prev_i = i;
+		if (arg[i] == '\'')
+			handle_single_quotes(arg, &i, queue);
+		else if (arg[i] == '\"')
+		{
+			values[0] = &i;
+			values[1] = exit_status;
+			handle_double_quotes(arg, values, queue, context);
+		}
+		else if (arg[i + 1] == '%')
+		{
+			enqueue_char(queue, '$');
+			i++;
+			while (arg[i] == '%')
+			{
+				enqueue_char(queue, '%');
+				i++;
+			}
+		}
+		else if (arg[i] == '$')
+		{
+			values[0] = &i;
+			values[1] = exit_status;
+			handle_dollar_sign(arg, values, queue, context);
+		}
+		else
+			enqueue_char(queue, arg[i++]);
+		if (prev_i == i && arg[i] != '\0')
+			i++;
+	}
+}
+
+static char	*clean_argument(char *arg, t_shell_context *context,
+		int *exit_status)
+{
+	t_queue_char	queue;
+	char			*cleaned_arg;
+	char			*home;
+
+	init_queue_char(&queue);
+	if (ft_strcmp(arg, "~") == 0)
+	{
+		home = get_env_value("HOME", context->env_vars);
+		if (home)
+			cleaned_arg = home;
+		else
+			cleaned_arg = ft_strdup("~");
+	}
+	else
+	{
+		process_argument(arg, &queue, exit_status, context);
+		cleaned_arg = queue_char_str_convert(&queue);
+	}
+	enqueue(&context->queue, cleaned_arg);
+	enqueue(&context->queue, " ");
+	return (cleaned_arg);
+}
+
+void	clean_nulls_from_argv(char **argv, int size)
+{
+	int		i;
+	int		j;
+	size_t	len;
+
+	i = 0;
+	j = 0;
+	while (i < size)
+	{
+		if (argv[i] != NULL)
+		{
+			if (argv[i][0] == '\x01' && argv[i][1] == '\0')
+			{
+				i++;
+				continue ;
+			}
+			if (argv[i][0] == '\x01')
+				ft_memmove(argv[i], argv[i] + 1, ft_strlen(argv[i]));
+			len = ft_strlen(argv[i]);
+			while (len > 0 && argv[i][len - 1] == '\x01')
+			{
+				argv[i][len - 1] = '\0';
+				len--;
+			}
+			argv[j] = argv[i];
+			j++;
+		}
+		i++;
+	}
+	while (j < size)
+	{
+		argv[j] = NULL;
+		j++;
+	}
+}
+
+// char	*remove_quotes(const char *str)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		in_double_quotes;
+// 	char	*result;
+// 	int		count;
+// 	int		k;
+
+// 	i = 0;
+// 	j = 0;
+// 	in_double_quotes = 0;
+// 	result = gc_malloc(strlen(str) + 1);
+// 	if (!result)
+// 		return (ft_strdup(""));
+// 	while (str[i] != '\0')
+// 	{
+// 		if (str[i] == '\"' && !in_double_quotes)
+// 		{
+// 			in_double_quotes = 1;
+// 			i++;
+// 		}
+// 		else if (str[i] == '\"' && in_double_quotes)
+// 		{
+// 			in_double_quotes = 0;
+// 			i++;
+// 		}
+// 		else if (str[i] == '\'' && !in_double_quotes)
+// 		{
+// 			if (str[i + 1] == '\'')
+// 			{
+// 				i += 2;
+// 				continue ;
+// 			}
+// 			count = 0;
+// 			k = i;
+// 			while (str[k] == '\'')
+// 			{
+// 				count++;
+// 				k++;
+// 			}
+// 			if (count > 1 && str[k] != '\0')
+// 			{
+// 				while (count > 0)
+// 				{
+// 					result[j++] = '\'';
+// 					i++;
+// 					count--;
+// 				}
+// 			}
+// 			else
+// 			{
+// 				i++;
+// 				while (str[i] && str[i] != '\'')
+// 					result[j++] = str[i++];
+// 				if (str[i] == '\'')
+// 					i++;
+// 			}
+// 		}
+// 		else
+// 			result[j++] = str[i++];
+// 	}
+// 	result[j] = '\0';
+// 	return (result);
+// }
+
+void	restore_quotes(char **vector)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (!vector)
+		return ;
+	i = 0;
+	while (vector[i])
+	{
+		str = vector[i];
+		j = 0;
+		while (str[j])
+		{
+			if (str[j] == '\x1F')
+				str[j] = '\'';
+			j++;
+		}
+		i++;
+	}
+}
+
+void	clean_execution_command_args(t_command *cmd, t_shell_context *context,
+		int *exit_status)
+{
+	int		i;
+	int		size;
+	t_exec	*exec;
+	char	*res;
+	char	**vector;
+
+	if (!cmd || !context)
+		return ;
+	exec = (t_exec *)cmd;
+	i = 0;
+	size = 0;
+	if (!exec->argv[0])
+		return ;
+	while (exec->argv[size])
+		size++;
+	while (exec->argv[i])
+	{
+		if (has_special_characters(exec->argv[i]))
+			exec->argv[i] = clean_argument(exec->argv[i], context, exit_status);
+		else
+		{
+			enqueue(&context->queue, exec->argv[i]);
+			enqueue(&context->queue, " ");
+		}
+		i++;
+	}
+	clean_nulls_from_argv(exec->argv, size);
+	res = queue_str_convert(&context->queue);
+	if (!res)
+		return ;
+	vector = ft_split_beta(res, ' ');
+	if (!vector)
+		return ;
+	clean_nulls_from_argv(vector, size);
+	i = 0;
+	while (vector[i])
+	{
+		vector[i] = remove_quotes(vector[i]);
+		i++;
+	}
+	restore_quotes(vector);
+	i = 0;
+	while (vector[i])
+	{
+		exec->argv[i] = vector[i];
+		i++;
+	}
+	exec->argv[i] = NULL;
+}
+
+void	process_all_commands(t_command *cmd, t_shell_context *context,
+		int *exit_status)
+{
+	t_pipe	*pipe;
+	t_redir	*redir;
+
+	if (cmd->type == CMD_EXEC)
+	{
+		clean_execution_command_args(cmd, context, exit_status);
+	}
+	else if (cmd->type == CMD_PIPE)
+	{
+		pipe = (t_pipe *)cmd;
+		process_all_commands(pipe->left, context, exit_status);
+		process_all_commands(pipe->right, context, exit_status);
+	}
+	else if (cmd->type == CMD_REDIR)
+	{
+		redir = (t_redir *)cmd;
+		if (redir->redir_type != '%' && has_special_characters(redir->file))
+		{
+			redir->file = clean_argument2(redir->file, context, exit_status);
+		}
+		process_all_commands(redir->sub_cmd, context, exit_status);
+	}
+}
+
+int	count_redirections(t_command *cmd)
+{
+	t_redir	*redir;
+	int		count;
+
+	count = 0;
+	if (cmd->type == CMD_REDIR)
+	{
+		redir = (t_redir *)cmd;
+		if (redir->redir_type == '%')
+			count++;
+		count += count_redirections(redir->sub_cmd);
+	}
+	return (count);
+}
---
+++ src/expander/files/expander_filename.d
@@ -0,0 +1,146 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expander_filename.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 23:39:15 by msennane          #+#    #+#             */
+/*   Updated: 2024/12/20 00:30:19 by msennane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+static void	handle_single_quotes2(char *str, int *index, t_queue_char *queue,
+		int *was_quoted)
+{
+	if (!str || !index || !queue || !was_quoted)
+		return ;
+	(*was_quoted) = 1;
+	(*index)++;
+	while (str[*index] && str[*index] != '\'')
+	{
+		enqueue_char(queue, str[*index]);
+		(*index)++;
+	}
+	if (str[*index] == '\'')
+	{
+		(*index)++;
+	}
+}
+
+static void	handle_dollar_in_quotes2(t_quote_params *params)
+{
+	char	*exit_status_str;
+	char	*var_name;
+	char	*var_value;
+
+	if (!params->arg[*params->i + 1] || is_whitespace(params->arg[*params->i
+			+ 1]) || params->arg[*params->i + 1] == '\"')
+	{
+		enqueue_char(params->q, '$');
+		(*params->i)++;
+		return ;
+	}
+	if (params->arg[*params->i + 1] == '?')
+	{
+		exit_status_str = ft_itoa(*params->exit_status);
+		if (exit_status_str)
+		{
+			enqueue_str(params->q, exit_status_str);
+			(*params->i) += 2;
+		}
+		return ;
+	}
+	(*params->i)++;
+	var_name = extract_variable_name(params->arg + *params->i);
+	if (var_name && (var_value = get_env_value(var_name,
+				params->context->env_vars)))
+		(enqueue_str(params->q, var_value), (*params->i)
+			+= ft_strlen(var_name));
+}
+
+static void	handle_double_quotes2(t_quote_params *params)
+{
+	if (!params->arg || !params->q || !params->context || !params->was_quoted)
+		return ;
+	(*params->was_quoted) = 1;
+	(*params->i)++;
+	while (params->arg[*params->i] && params->arg[*params->i] != '\"')
+	{
+		if (params->arg[*params->i] == '$')
+			handle_dollar_in_quotes2(params);
+		else
+		{
+			enqueue_char(params->q, params->arg[*params->i]);
+			(*params->i)++;
+		}
+	}
+	if (params->arg[*params->i] == '\"')
+		(*params->i)++;
+}
+
+static void	process_argument2(char *arg, t_queue_char *queue, int *exit_status,
+		t_shell_context *context, int *was_quoted)
+{
+	int				i;
+	int				*values[2];
+	int				prev_i;
+	t_quote_params	params;
+
+	if (!arg || !queue || !exit_status || !context || !was_quoted)
+		return ;
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		prev_i = i;
+		values[0] = &i;
+		values[1] = exit_status;
+		if (arg[i] == '\'')
+			handle_single_quotes2(arg, &i, queue, was_quoted);
+		else if (arg[i] == '\"')
+		{
+			params.arg = arg;
+			params.i = values[0];
+			params.q = queue;
+			params.context = context;
+			params.was_quoted = was_quoted;
+			handle_double_quotes2(&params);
+		}
+		else if (arg[i] == '$')
+			handle_dollar_sign(arg, values, queue, context);
+		else
+			enqueue_char(queue, arg[i++]);
+		if (prev_i == i && arg[i] != '\0')
+			i++;
+	}
+}
+
+char	*clean_argument2(char *arg, t_shell_context *context, int *exit_status)
+{
+	t_queue_char	queue;
+	char			*cleaned_arg;
+	char			*home;
+	int				was_quoted;
+
+	was_quoted = 0;
+	init_queue_char(&queue);
+	if (ft_strcmp(arg, "~") == 0)
+	{
+		home = get_env_value("HOME", context->env_vars);
+		if (home)
+			cleaned_arg = home;
+		else
+			cleaned_arg = ft_strdup("~");
+	}
+	else
+	{
+		process_argument2(arg, &queue, exit_status, context, &was_quoted);
+		cleaned_arg = queue_char_str_convert(&queue);
+	}
+	if (is_ambiguous_redirect(cleaned_arg) && !was_quoted)
+		(ft_putstr_fd("minishell: ambiguous redirect\n", STDERR_FILENO),
+			cleaned_arg = ft_strdup("\x01\x01\x01"));
+	return (cleaned_arg);
+}
