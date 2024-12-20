af6efd3aee91ff01703f4291338d5e3f2f458b4d
mouadse <mouadse@gmail.com>
2024-12-20 23:55:04 +0100

    X

--- src/expander/remove_quotes.c
+++ src/expander/remove_quotes.c
@@ -6,7 +6,7 @@
 /*   By: msennane <msennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 23:34:10 by msennane          #+#    #+#             */
-/*   Updated: 2024/12/20 23:53:43 by msennane         ###   ########.fr       */
+/*   Updated: 2024/12/20 23:54:59 by msennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,11 +84,12 @@ char	*remove_quotes(const char *str)
 	size_t	result_index;
 	bool	in_double_quotes;
 	size_t	i;
+	char	*result;
 
 	if (str == NULL)
 		return (NULL);
 	len = strlen(str);
-	char *result = gc_malloc(len + 1);
+	result = gc_malloc(len + 1);
 	if (!result)
 		return (ft_strdup(""));
 	result_index = 0;
@@ -97,13 +98,16 @@ char	*remove_quotes(const char *str)
 	while (i < len)
 	{
 		if (str[i] == '"')
+		{
 			in_double_quotes = !in_double_quotes;
+			/* Don't add the double quote to result */
+		}
 		else if (str[i] == '\'' && !in_double_quotes)
 		{
+			/* If next char is also a quote, skip both */
 			if (i + 1 < len && str[i + 1] == '\'')
 				i++;
-			else
-				result[result_index++] = str[i];
+			/* Don't add single quotes to the result */
 		}
 		else
 			result[result_index++] = str[i];
